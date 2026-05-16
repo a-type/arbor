1f81cf89cd244089ad18c5301bba2bd298cb59d1
Grant Forrest <gaforres@gmail.com>
2026-05-15 22:32:40 -0400

    feat: rule to turn reverse shadow off

--- packages/classes/src/rules/shadow.ts
+++ packages/classes/src/rules/shadow.ts
@@ -69,6 +69,12 @@ export const shadowRules: Rule<Theme>[] = [
 		}),
 	],
 	[
+		/^shadow-reverse-off$/,
+		() => ({
+			[$systemProps.dynamic.shadowReverse.name]: '1',
+		}),
+	],
+	[
 		/^shadow-none$/,
 		() => ({
 			[$classesProps.shadow.shadow.name]: '0 0 rgb(0 0 0 / 0)',
