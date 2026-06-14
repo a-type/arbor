e1f79264d5daefaddcf05a7c89771eb9ba7f9275
Grant Forrest <gaforres@gmail.com>
2026-06-13 22:29:26 -0400

    ci: not sure this is necessary

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -5,4 +5,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	integrations: [mdx()],
+	base: import.meta.env.BASE_PATH || '/',
+	trailingSlash: 'never',
+	site: import.meta.env.SITE,
 });
